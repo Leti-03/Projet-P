4818463bacec8ecbb26cb9a0fb34956e510ed50b
anais38 <anais.fellahi@ummto.dz>
2026-03-27 19:36:36 +0100

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './context/AuthContext';   // ← doit correspondre
 import ClientReclamation from './pages/client/Reclamation';
 import SuiviReclamation from './pages/client/SuiviReclamation';
 import Historique from './pages/client/Historique';
+import Dashboard from './pages/client/Dashboard';
 
 export default function App() {
   return (
@@ -12,12 +13,14 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           {/* Redirection par défaut */}
-          <Route path="/" element={<Navigate to="/client/historique" replace />} />
+          <Route path="/" element={<Navigate to="/client/dashboard" replace />} />
 
           {/* Pages Client */}
           <Route path="/client/reclamation" element={<ClientReclamation />} />
           <Route path="/client/suivi"       element={<SuiviReclamation />} />
           <Route path="/client/historique"  element={<Historique />} />
+          <Route path="/client/dashboard" element={<Dashboard />} />
+
 
           {/* Page 404 */}
           <Route path="*" element={<Navigate to="/client/historique" replace />} />
